56d92ee0053c1a6076a719aac4c024b723696a6f
RasputinForever <erik.justin.portillo@gmail.com>
2021-02-02 23:51:49 -0800

    adds initial server js noteTaker.js with start code

---
+++ noteTaker.js
@@ -0,0 +1,46 @@
+// starting off...
+    // need an express server that servs up the given index.html, notes.html
+        // delivers CSS and JS files with those... HOW? Google!
+    
+    // both pages have functionality that write data back to the JSON string
+        // write to requires... all info provided AND a unique key
+            // if we're going off the dome, the key can = something based on the exact moment of submission in milliseconds or something like that
+
+    // where is the JSON file served and saved from? Local file directory. 
+        // fs stuff needs to be on its own .js file called "db.js" and will be async because reading/writing is just nice that way.
+
+    // step 1: outline the IN and OUTs
+    // step 2: connect to inputs on given pages
+
+
+
+const express = require("express");
+const path = require("path");
+
+
+const app = express();
+const PORT = 4000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// connect this to fs eventually
+// const savedNotes = getSavedNotes();
+const savedNotes = [
+    {
+        title: "Test Title",
+        text: "Test text",
+        id: 1234
+    }
+];
+
+
+//html routes
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./Develop/public/index.html")));
+app.get("/home", (req, res) => res.sendFile(path.join(__dirname, "./Develop/public/index.html")));
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "./Develop/public/notes.html")));
+
+// put post stuff here
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
